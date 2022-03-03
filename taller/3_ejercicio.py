305d5d1b8d59343476d3e03b44bf837c51dd4ac0
= <jdiaz89@cuc.edu.co>
2022-03-02 19:57:12 -0500

    tercer ejercicio

--- taller/3_ejercicio.py
+++ taller/3_ejercicio.py
@@ -4,3 +4,8 @@ total obtendrá por las ventas que realiza en el mes. Desarrolle un programa
 en Python que permita mostrar el valor ganado por comisión y el valor total
 a pagar al vendedor.''' 
 
+sueldo_vendedor = float(input("Ingrese el sueldo base del vendedor: "))
+
+venta = float(input("Ingrese el valor total de las ventas: "))
+comision_ventas = venta * 0.15
+print(f'El valor ganado por las comisiones es de {comision_ventas} y el valor total a pagar es de {sueldo_vendedor + comision_ventas}')
